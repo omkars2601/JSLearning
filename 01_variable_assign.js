0ae2a09c671292ce1be4283bd69e4dbb913d7771
omkars2601 <omkars2601@gmail.com>
2023-02-16 18:53:29 +0530

    variable declaration
    variable declaration

---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+console.log("****Step 1****");
+var myFullName
+console.log(myFullName);
+myFullName= "omkar sonawane";
+console.log(myFullName);
+myFullName="omkar chandrakant sonawane";
+console.log(myFullName);
+console.log("****Step 2****");
+var myLove ="I love JS";
+console.log(myLove);
+console.log("****Step 3****");
+var FirstName="Omkar";
+var lastName="Sonawane";
+var age="22";
+var address="\nShirur,Pune";
+var pincode="412210";
+var gender="\nMale";
+var isMarried="Married";
+console.log(FirstName,lastName,age,address,pincode,gender,isMarried);
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
 console.log("Omkar Sonawane")
+console.log("31");
+console.log("Pune");
+
+var fullName="Omkar Sonawane"
+console.log(fullName);
+console.log(fullName);
+
+var city;    //variable Declaration 
+console.log(city);
+city="Pune";  //variable initialization
+console.log(city);
+city="Surat";
+console.log(city);
