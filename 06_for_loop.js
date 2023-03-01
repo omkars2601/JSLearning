a840530dd9de29f8a72cc1885831538e7b499561
omkars2601 <omkars2601@gmail.com>
2023-03-01 15:10:20 +0530

    for loop
    for loop

---
+++ 06_for_loop.js
@@ -0,0 +1,21 @@
+//print nums 1 to 10
+//initilization 1
+//update expression
+//condition <=10
+for (let index = 1; index <= 10; index = index + 1) {
+  console.log(index);
+}
+
+for (let index = 0; index <= 5; index = index + 1) {
+  console.log(index);
+}
+
+console.log("Reverse number");
+for (let index = 10; index >= 0; index = index - 1) {
+  console.log(index);
+}
+
+console.log(`Table of 5`);
+for (let index = 5; index <= 50; index = index + 5) {
+  console.log(index);
+}
