9bec4088e567a50d348b0ccbf952ed52dec13bc4
omkars2601 <omkars2601@gmail.com>
2023-03-10 17:43:57 +0530

    corrected file with return statement
    corrected file with return statement

--- 07_factorial_ass01.js
+++ 07_factorial_ass01.js
@@ -1,6 +1,6 @@
 function factorialOfNum(num) {
   if (typeof num != "number" || isNaN(num) || num <= 0) {
-    console.log(`Invaid input : ${num}`);
+     return `Invaid Number: ${num}`
   } else {
     let fact = 1;
     for (let index = num; index >= 1; index--) {
@@ -8,6 +8,7 @@ function factorialOfNum(num) {
     }
     return `Factorial of number: ${num} is ${fact}`;
   }
+  
 }
 
 let re1 = factorialOfNum(5);
@@ -15,9 +16,11 @@ console.log(re1);
 let re2 = factorialOfNum(3);
 console.log(re2);
 let re3 = factorialOfNum(null);
+console.log(re3);
 let re4 = factorialOfNum(8);
 console.log(re4);
 let re5 = factorialOfNum(undefined);
+console.log(re5);
 let re6 = factorialOfNum(9);
 console.log(re6);
 let re7 = factorialOfNum(0);
